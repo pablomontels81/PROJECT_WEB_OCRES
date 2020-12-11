0572d56390f26a432c86731eadf10f81ed86e903
pablomontels81 <pablo.montels@edu.ece.fr>
2020-12-11 16:46:43 +0100

    Léger changement

--- backend/routes/goal.js
+++ backend/routes/goal.js
@@ -17,20 +17,59 @@ router.route('/_id').get((req,res) =>{
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-/* POST update des goals (Widget Update Goal)*/
-router.post('/update/:id', (req, res) =>{
-    Goal.findById(req.params.id)
+/* POST update du goal velo (Widget Update Goal)*/
+router.post('/update_velo', (req, res) =>{
+    Goal.find()
     .then(goal => {
         goal.velo_goal = req.body.velo_goal;
-        goal.natation_goal = req.body.natation_goal;
-        goal.course_goal = req.body.course_goal;
 
         goal.save()
-            .then(() => res.json('Goal updated !'))
+            .then(() => res.json('Goal Velo updated !'))
             .catch(err => res.status(400).json('Error: '+err));
     })
     .catch(err => res.status(400).json('Error: '+err));
-})
+});
+
+/* POST update du goal course (Widget Update Goal)*/
+router.post('/update_course', (req, res) =>{
+    Goal.find()
+    .then(goal => {
+        goal.course_goal = req.body.course_goal;
+
+        goal.save()
+            .then(() => res.json('Goal Course updated !'))
+            .catch(err => res.status(400).json('Error: '+err));
+    })
+    .catch(err => res.status(400).json('Error: '+err));
+});
+
+/* POST update du goal natation (Widget Update Goal)*/
+router.post('/update_natation', (req, res) =>{
+    const filter = {_id : "5fd21280bca1f33ea277a864"};
+    const update = {natation_goal : req.body.natation_goal};
+    Goal.findOneAndUpdate(filter,update)
+        .then(() => res.json('Goal Natation updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+/* POST update du goal course (Widget Update Goal)*/
+router.post('/update_course', (req, res) =>{
+    const filter = {_id : "5fd21280bca1f33ea277a864"};
+    const update = {course_goal : req.body.course_goal};
+    Goal.findOneAndUpdate(filter,update)
+        .then(() => res.json('Goal Course updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+/* POST update du goal velo (Widget Update Goal)*/
+router.post('/update_velo', (req, res) =>{
+    const filter = {_id : "5fd21280bca1f33ea277a864"};
+    const update = {velo_goal : req.body.velo_goal};
+    Goal.findOneAndUpdate(filter,update)
+        .then(() => res.json('Goal Velo updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
 
 router.post('/add',(req, res) => {
     const velo_goal = String(req.body.velo_goal);
